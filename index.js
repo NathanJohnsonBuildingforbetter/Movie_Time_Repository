cc812b6df11899d87c2948429846c434d4c97913
NathanJohnsonBuildingforbetter <NathanJohnsonbuildingforbetter@gmail.comecho>
2026-06-15 13:15:42 -0400

    solving bugs

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=56fd1ab8
+
+const movieTitleInput = document.getElementById("movie-title");
+const submitButton = document.getElementById("submitBtn");
+
+
+
+submitButton.addEventListener("click", function(){
+    const movieTitleText = movieTitleInput.value;
+
+    console.log(movieTitleText)
+    console.log(movieTitleInput)
+});
+
+
+
+function searchForMovie(movieTitleText){
+    console.log(movieTitleText)
+
+}
