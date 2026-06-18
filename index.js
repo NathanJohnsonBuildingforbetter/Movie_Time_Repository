71a0d69e59bc548f8d6d407fd5434e664babd319
NathanJohnsonBuildingforbetter <NathanJohnsonbuildingforbetter@gmail.comecho>
2026-06-18 07:03:33 -0400

    solving bugs

--- index.js
+++ index.js
@@ -8,13 +8,21 @@ const submitButton = document.getElementById("submitBtn");
 submitButton.addEventListener("click", function(){
     const movieTitleText = movieTitleInput.value;
 
-    console.log(movieTitleText)
-    console.log(movieTitleInput)
+    // console.log(movieTitleText)
+    // console.log(movieTitleInput)
+    searchForMovie(movieTitleText)
 });
 
 
 
-function searchForMovie(movieTitleText){
-    console.log(movieTitleText)
+async function searchForMovie(movieTitleText){
+    console.log("Search for:", movieTitleText)
+
+    const movieTitleValue = movieTitleText
+    const moviesPromises = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=56fd1ab8&s=${movieTitleText}`)
+    console.log(moviesPromises)
+    const moviePromisesData = await moviesPromises.json()
+    console.log(moviesPromises.json())
 
 }
+searchForMovie(movieTitleText)
